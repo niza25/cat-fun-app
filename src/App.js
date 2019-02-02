b311f8b4e6f87452aa1a8ffc58e023623aa4b3b0
Izabela <izabela.nowosada@gmail.com>
2019-02-02 09:08:52 +0100

    Configure store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import {Provider} from 'react-redux'
+import store from './store'
 import './App.css';
 import ImgFactContainer from './components/ImgFactContainer'
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
        <ImgFactContainer />
       </div>
+      </Provider>
     );
   }
 }
--- src/components/ImgFactContainer.js
+++ src/components/ImgFactContainer.js
@@ -10,3 +10,5 @@ export default class ImgFactContainer extends React.Component {
 
 }
 
+
+
